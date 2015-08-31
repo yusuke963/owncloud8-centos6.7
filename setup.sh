e7da8c72ed0413d20c1b0de2b37c31e2b47f5339
Yusuke Suzuki <yusuke.home307@gmail.com>
2015-08-31 10:39:55 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+yum install -y git epel-release ansible git
+yum makecache
+
+mkdir ~/src/
+cd ~/src
+rm -rf owncloud8-lamp-softlayer
+unset SSH_ASKPASS
+git clone https://github.com/yusuke963/owncloud8-centos6.7.git
+cd owncloud8-centos6.7
+ansible-playbook -i host install.yml
